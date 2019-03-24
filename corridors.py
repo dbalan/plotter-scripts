ba04a8dfcfab8f81585fbe5c9660359ea6a7ee12
wildconceits <alexanderjyeh@gmail.com>
2019-03-24 19:38:24 -0400

    Messily created more complex grid of polygons
    Draws pleasingly dense and complex array of polygons and squares.

--- corridors.py
+++ corridors.py
@@ -4,31 +4,11 @@ import random
 import serial
 import math
 
-def circle_points(cx, cy, radius, rotation=0, arc=5):
-    """Generates 360/arc points on the circumference of a circle"""
-    steps = int(360.0 / arc) + 1
-    for deg in np.linspace(0 + rotation, 360 + rotation, steps):
-        yield (int(cx + radius * math.cos(deg * 2 * math.pi / 360)),
-               int(cy + radius * math.sin(deg * 2 * math.pi / 360)))
+import common
+from bday import text
+from polygons_r_circles import circle_points, circle
 
-def circle(cx, cy, radius, rotation=0, arc=5):
-    current = circle_points(cx, cy, radius, rotation=rotation, arc=arc)
-    instructions = []
-
-    (first_x, first_y) = next(current)
-    instructions.append("PA{},{};".format(first_x, first_y))
-    for (x, y) in current:
-        instructions.append("PD{},{};".format(x, y))
-
-    instructions.append("PD{},{};".format(first_x, first_y))
-    instructions.append("PU;")
-    return instructions
-
-def square(cx, cy, radius, rotation=45):
-    """Draws square centered at (cx, cy) with side length sqrt(2)* radius"""
-    return circle(cx, cy, radius, rotation=rotation, arc=90)
-
-def hallway(cx, cy, radius, disp_x, disp_y, end_rad, end_rot = 45, steps=15):
+def hallygon(cx, cy, radius, sides, disp_x, disp_y, end_rad, end_rot, steps = 15):
     """Draws steps number of squares interpolated between start and end"""
     instructions = []
     x_series = np.geomspace(cx, cx + disp_x, steps)
@@ -37,34 +17,54 @@ def hallway(cx, cy, radius, disp_x, disp_y, end_rad, end_rot = 45, steps=15):
     rot_series = np.linspace(45, end_rot, steps)
 
     for (x, y, rad, rot) in zip(x_series, y_series, rad_series, rot_series):
-        instructions.extend(square(x, y, rad, rotation=rot))
+        instructions.extend(circle(x, y, rad, rotation=rot, arc = 360/sides))
 
     return instructions
 
-port = "/dev/ttyUSB0"
-speed = 9600
+def is_prime(num):
+    """Tests if num is prime"""
+    if num == 0 or num == 1:
+        return False
+    bound = int(math.sqrt(num)) + 1
 
-with serial.Serial(port, speed, timeout=None) as plt:
-#with open("hallway.hpgl", "w")  as plt:
-    #send initialization instructions
-    plt.write("IN;PU;SP1;")
+    for i in range(2, num + 1):
+        for j in range(2, bound):
+            if i * j == num:
+                return False
+    return True
 
-    deviation = (-300, -100, 0, 100, 300)
-    rotation = (0, 29, 45, 78, 120)
-    spacing = 1500
-    for x, dev in enumerate(deviation):
-        for y, rot in enumerate(rotation):
-            figure = hallway(750 + y * spacing, 750 + x * spacing, 1000, 
-                             -dev, dev, 250,
-                             end_rot = rot)
-            for line in figure:
-                plt.write(line)
-                
-                #send a blocking isnstruction, to know when pen has stopped
-                plt.write("OA;")
-                c = ""
-                while c != "\r":
-                    c = plt.read()
+if __name__ == "__main__":
+    port = "/dev/ttyUSB0"
+    speed = 9600
+    instructions = []
 
-    #Finally, put back the pen
-    plt.write("SP0;")
+    DEFAULT_SIZE = 575
+    DEV = 250
+    size = DEFAULT_SIZE
+    spacing = size/(math.sqrt(2))
+    margin = size + DEV
+    instructions.extend(text("hallygons", size - 200, size - 200))
+    deviation_x = np.linspace(-DEV, DEV, 23)
+    deviation_y = np.linspace(-DEV, DEV, 14)
+    for x, dev_x in enumerate(deviation_x):
+        for y, dev_y in enumerate(deviation_y):
+            cx = margin + x * spacing
+            cy = margin + y * spacing
+
+            if is_prime(x + 1) and is_prime(y + 1):
+                sides = max([x + 1, y + 1])
+                instructions.append("SP2;")
+                size = size / math.sqrt(2)
+                rot = (360 / (sides / math.pi))
+            else:
+                sides = 4
+                instructions.append("SP1;")
+                size = DEFAULT_SIZE
+                rot = 157.5
+
+            instructions.extend(hallygon(cx, cy, size, sides,
+                                         dev_x, dev_y, 60,
+                                         end_rot = rot, steps = 4))
+
+    common.exec_hpgl(instructions, port=port, speed=speed)
+    #common.write_hpgl(instructions, "hallway.hpgl")
